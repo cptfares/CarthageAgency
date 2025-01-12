ef25ca2072b5ba85bb5363c7155508bdd4ce233a
cptfares <itsfares11@gmail.com>
2025-01-12 13:36:21 +0100

    enable production browser source maps

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,12 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Head from "@/node_modules/next/head";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-      <Head>
-      <title>Carthage Agency</title>
-      </Head>
+
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
